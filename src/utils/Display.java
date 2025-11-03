29d88d67f90e78b5886fbe63f32db6095c23e5f5
Luiz Nascimento <luizfelipesilvadonascimento@gmail.com>
2025-11-03 08:38:40 -0300

    fix: display help fix color param example

--- src/utils/Display.java
+++ src/utils/Display.java
@@ -37,7 +37,7 @@ public class Display {
     return "Usage: java -jar ascii-art-converter.jar -i <image_path> [-color <color>] [-filter <filter_method>]\n" +
          "Options:\n" +
          "  -i, --image <image_path>   Path to the image file to convert.\n" +
-         "  -color <color>             Output color for the ASCII art (default: ANSI_WHITE).\n" +
+         "  -color <color>             Output color for the ASCII art (default: white).\n" +
          "      Available colors: red, green, yellow, blue, purple, cyan, white\n" +
          "  -filter <filter_method>    Filter method to apply (e.g., 'nearest', 'bilinear').\n" +
          "  -h, --help                 Display this help message.";
